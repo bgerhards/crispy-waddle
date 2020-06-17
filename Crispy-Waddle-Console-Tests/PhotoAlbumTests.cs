4333fd3d0da65e811368b4f1a69c4f7868114cce
Brian Gerhards <briangerhards@gmail.com>
2020-06-17 10:32:28 -0500

    base program from template. Added canary test

---
+++ Crispy-Waddle-Console-Tests/PhotoAlbumTests.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace Crispy_Waddle_Console_Tests
+{
+    public class PhotoAlbumTests
+    {
+
+        [Fact]
+        public void CanaryTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ Crispy-Waddle-Console/PhotoAlbum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Crispy_Waddle_Console
+{
+    public class PhotoAlbum
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
